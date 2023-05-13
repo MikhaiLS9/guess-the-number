42fa24830871a844b190cff05bc0ed1a40dbd7d4
MikhaiLS9 <famm93@mail.ru>
2023-05-13 23:19:13 +0300

    1

---
+++ main/app.js
@@ -0,0 +1,33 @@
+function randomInteger(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+function game() {
+  let randomNumber = randomInteger(1, 100);
+  function guessNumber() {
+    let userNumber = +prompt("Угадай число от 1 до 100");
+    if (userNumber === null) {
+      alert("Игра окончена");
+      return;
+    }
+
+    if (isNaN(userNumber)) {
+      alert("Введи число!");
+      guessNumber();
+      return;
+    }
+
+    if (userNumber > randomNumber) {
+      alert(`Загаданное число меньше`);
+      guessNumber();
+    } else if (userNumber < randomNumber) {
+      alert("Загаданное число больше");
+      guessNumber();
+    } else {
+      alert("Поздравляю, Вы угадали!!!");
+      return;
+    }
+  }
+
+  return guessNumber();
+}
+game();
