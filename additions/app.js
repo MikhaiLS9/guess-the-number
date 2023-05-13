33d107f27f8da8a015ec5495e52fdd95d130863b
MikhaiLS9 <famm93@mail.ru>
2023-05-13 23:42:55 +0300

    2

--- additions/app.js
+++ additions/app.js
@@ -2,9 +2,11 @@ function randomInteger(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 function game() {
+  let count = 9;
   let randomNumber = randomInteger(1, 100);
   function guessNumber() {
-    let userNumber = +prompt("Угадай число от 1 до 100");
+    let userNumber = prompt("Угадай число от 1 до 100");
+
     if (userNumber === null) {
       alert("Игра окончена");
       return;
@@ -15,17 +17,24 @@ function game() {
       guessNumber();
       return;
     }
-
+    userNumber = Number(userNumber)
+    if(count > 0){
     if (userNumber > randomNumber) {
-      alert(`Загаданное число меньше`);
+      alert(`Загаданное число меньше. Осталось попыток ${count}`);
+      count--;
       guessNumber();
     } else if (userNumber < randomNumber) {
-      alert("Загаданное число больше");
+      alert(`Загаданное число больше. Осталось попыток ${count}`);
+      count--;
       guessNumber();
     } else {
       alert("Поздравляю, Вы угадали!!!");
       return;
     }
+    alert("Игра окончена");
+    
+    return game()
+}
   }
 
   return guessNumber();
